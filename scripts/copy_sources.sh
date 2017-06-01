d9482a038838b1792a93d90193d356ac9e1e0fa5
paynd <mrpaynd@gmail.com>
2017-06-01 12:20:55 +0300

    copy script`s pathes fixed

--- scripts/copy_sources.sh
+++ scripts/copy_sources.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd ..
-cp -r ./example/node_modules/tipsi-stripe/android/src ./android/src
-cp -r ./example/node_modules/tipsi-stripe/src ./src
+cp -r ./example/node_modules/tipsi-stripe/android/src ./android
+cp -r ./example/node_modules/tipsi-stripe/src ./
